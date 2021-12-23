a5769fa7b62221a1eb885be06fe7fc0828af0456
SamuraiMe <samurai7@foxmail.com>
2021-12-23 11:42:57 +0800

    Modified `platforms` and `swift-tools-version`

--- Package.swift
+++ Package.swift
@@ -1,28 +1,36 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+  // swift-tools-version:5.4
+  // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "ObservableForm",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "ObservableForm",
-            targets: ["ObservableForm"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ObservableForm",
-            dependencies: []),
-        .testTarget(
-            name: "ObservableFormTests",
-            dependencies: ["ObservableForm"]),
-    ]
+  name: "ObservableForm",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "ObservableForm",
+      targets: ["ObservableForm"]
+    ),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "ObservableForm",
+      dependencies: []
+    ),
+    .testTarget(
+      name: "ObservableFormTests",
+      dependencies: ["ObservableForm"]
+    ),
+  ]
 )
